8b0a8551ee814e625073c239f194da6c872614ff
Samuel Wright <wsright987@gmail.com>
2021-06-22 07:35:00 -0400

    update

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,26 +2,12 @@ import React, { useContext, useEffect, useState } from "react";
 import "./style.css";
 import { Footer, Header, Navbar } from "../../containers";
 import { MatchupTable, HeroProfile, RandomHero } from "../../components";
-import stateManager from "../../js/stateManager";
 
 export default function Home() {
-  const [alert, setAlert] = useState(true);
-  
-  useEffect(()=>{
-    //console.log(stateManager.game);
-  })
-
   return (
     <div className="home">
         <Header />
-        <div style={{
-          width: '90%',
-          maxWidth: '1080px',
-          backgroundColor: 'white',
-          boxShadow: '0px 0px 5px .5px rgba(153, 153, 153, 0.4)',
-          padding: '10px 15px',
-          margin: 'auto'
-        }}>
+        <div className='home__body'>
           <RandomHero />
           <hr class="home-hr"/>
           <HeroProfile />
