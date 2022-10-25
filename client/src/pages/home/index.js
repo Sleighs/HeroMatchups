1a9ecbf3eabcb0a5f9a5587ce934aea92039551b
Samuel Wright <wsright987@gmail.com>
2022-10-25 17:04:27 -0400

    Favicon update

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,16 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./style.css";
 import { Footer} from "../../containers";
 import { MatchupTable, RandomHero, HeroSelection } from "../../components";
 import { RequestContext } from "../../contexts/RequestContext";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import DarkModeToggle from "react-dark-mode-toggle";
 
 const Home = () => {
   const { getAllHeroes, heroData } = useContext(RequestContext)
   const { theme, setTheme } = useContext(ThemeContext)
+  
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   var reqCount = 0;
 
@@ -20,17 +23,21 @@ const Home = () => {
   return (
     <div className="home">
         <div className='home__body'>
-          <div className='theme-toggle' 
-          style={{display: 'none', float: 'right'}}
-            onClick={()=>{
+
+          <DarkModeToggle
+            className="themeToggleBtn"
+            onChange={() => {
               if (theme === 'dark-theme'){
                 setTheme('light-theme')
+                setIsDarkMode(false)
               } else {
                 setTheme('dark-theme')
+                setIsDarkMode(true)
               }
-            }}>
-            {theme === 'dark-theme' ? 'light on' : 'light off'}
-          </div>
+            }}
+            checked={isDarkMode}
+            size={40}          
+          />
           
           <div className='page-header__container'>
             <h2 className={`page-header__title ${theme}__title`}>
