a99f76a5e9bef5f84f77e57241be8771771b4f56
Samuel Wright <wsright987@gmail.com>
2023-08-28 17:16:46 -0400

    Added svg's

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,11 @@ import { RequestContextProvider } from './contexts/RequestContext';
 import { ThemeContextProvider } from './contexts/ThemeContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RequestContextProvider>
-      <ThemeContextProvider>
-        <App />
-      </ThemeContextProvider>
-    </RequestContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RequestContextProvider>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </RequestContextProvider>,
+document.getElementById('root')
 );
 
