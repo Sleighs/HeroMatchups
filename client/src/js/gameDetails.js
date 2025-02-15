fea4fdb06c4f2929908daf831a273e6851259119
Samuel Wright <wsright987@gmail.com>
2025-02-14 23:53:59 -0500

    Updated game details

--- client/src/js/gameDetails.js
+++ client/src/js/gameDetails.js
@@ -1,8 +1,8 @@
 const gameDetails = {
   game: 'Overwatch 2',
-  date: 'September 2024',
+  date: 'January 2025',
   version: 4,
-  season: 12,
+  season: 15,
   upcoming: ['Venture', 'Juno', 'Hazard'],
 }
         
